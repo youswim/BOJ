bc7bd976aaa26cc29725e4fa458e943a0d69564b
youswim <78892152+youswim@users.noreply.github.com>
2022-03-26 23:53:49 +0900

    Add files via upload

---
+++ BOJ_1225.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        StringTokenizer st;
+
+        st = new StringTokenizer(input);
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        long aTot = 0;
+        long bTot = 0;
+
+        for (int i = 0; i < A.length(); i++) {
+            aTot += Character.getNumericValue(A.charAt(i));
+        }
+        for (int j = 0; j < B.length(); j++){
+            bTot += Character.getNumericValue(B.charAt(j));
+        }
+
+        System.out.println(aTot * bTot);
+    }
+}
