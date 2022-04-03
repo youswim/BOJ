fd2ff7309d3ba15a2f475bdf7b3f954fb5a74e93
youswim <78892152+youswim@users.noreply.github.com>
2022-04-03 19:46:44 +0900

    Add files via upload

--- BOJ_10844.java
+++ BOJ_10844.java
@@ -3,46 +3,63 @@ package hellojpa;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
-    final static long mod = 1000000000;
+    static int N;
+    static final int TEN = 10;
+    static final int BILLION = 1_000_000_000;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
 
+        getInput();
+
+        setDp();
+
+        calcDp();
+
+        getRes();
+
+    }
+
+    static void getInput() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][TEN];
+        br.close();
+    }
 
-        long[][] dp = new long[N + 1][10];
-
-        for(int i = 1; i < 10; i++) {
+    static void setDp() {
+        for (int i = 1; i < TEN; i++) {
             dp[1][i] = 1;
         }
+    }
 
-        for(int i = 2; i <= N; i++) {
+    static void calcDp() {
 
-            for(int j = 0; j < 10; j++) {
-
-                if(j == 0) {
-                    dp[i][0] = dp[i - 1][1] % mod;
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < TEN; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i-1][j + 1] % BILLION;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i-1][j - 1] % BILLION;
                 }
-                else if (j == 9) {
-                    dp[i][9] = dp[i - 1][8] % mod;
-                }
-                else {
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
-                }
+                else
+                    dp[i][j] = (dp[i-1][j - 1] + dp[i-1][j + 1]) % BILLION;
             }
         }
+    }
 
-        long result = 0;
-
-        for(int i = 0; i < 10; i++) {
-            result += dp[N][i];
+    static void getRes() {
+        int res = 0;
+        for (int i = 0; i < TEN; i++) {
+            res += dp[N][i];
+            res %= BILLION;
         }
-
-        System.out.println(result % mod);
+        System.out.println(res);
     }
 
 }
