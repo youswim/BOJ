002b05ea258729aebf14d6accd286cffb144e5fe
youswim <78892152+youswim@users.noreply.github.com>
2022-04-04 22:17:48 +0900

    Add files via upload

---
+++ BOJ_11053.java
@@ -0,0 +1,43 @@
+package hellojpa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        String[] str = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = i; j >= 0; j--) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        System.out.println(max);
+
+    }
+}
