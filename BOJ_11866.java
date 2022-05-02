36f2080a1aa84b88666f34c4417d25a2176666a1
youswim <78892152+youswim@users.noreply.github.com>
2022-05-03 01:49:32 +0900

    Update BOJ_11866.java

--- BOJ_11866.java
+++ BOJ_11866.java
@@ -1,43 +1,34 @@
-//V
+package main;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		for(int i = 1; i <= N; i++) {
-			list.add(i);
-		}
-		
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-		
-		int index = 0;	// 리스트에서 삭제할 요소를 참조할 인덱스 변수
-		
-		while(N > 1) {
-			index = (index + (K - 1)) % N;
-			
-			// index위치에 있는 요소를 삭제함과 동시에 출력 
-			sb.append(list.remove(index)).append(", "); 
-			N--;
-		}
-		
-		// 마지막으로 남은 요소 삭제함과 동시에 출력
-		sb.append(list.remove()).append('>');
-		System.out.println(sb);
-	}
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("<");
+        String[] str = br.readLine().split(" ");
+
+        int N = Integer.parseInt(str[0]);
+        int K = Integer.parseInt(str[1]);
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        int idx = 0;
+        while (N > 1) {
+            idx = (idx + (K - 1)) % N;
+            sb.append(list.remove(idx)).append(", ");
+            N--;
+        }
+        sb.append(list.remove()).append(">");
+        System.out.println(sb);
+    }
 }
